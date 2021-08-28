746c33905b00e22c02151d2055f01c873fc0d822
lukaszdadura <83578617+lukaszdadura@users.noreply.github.com>
2021-08-28 11:26:32 +0200

    commit

--- src/main/resources/task15.sql
+++ src/main/resources/task15.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(*) FROM employees WHERE manager_id IS NOT NULL;
+SELECT COUNT(*) FROM employees WHERE manager_id IS NULL;
---
+++ src/test/java/Task9Test.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task9Test {
+
+    @Test
+    void isAnagram() {
+    }
+
+    @Test
+    void isNotAnagram() {
+    }
+
+    @Test
+    void isAnagramUpperLowerCase() {
+    }
+
+    @Test
+    void isAnagramWithNull() {
+    }
+}
